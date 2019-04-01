7cf9dbd600bd1e52e6fe12c0b38d14e5727cd7c1
peter-keller <keller.f.peter@gmail.com>
2019-04-01 13:34:35 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+    get '/' do 
+        "Hello, World!"
+    end
+
+    get '/name' do 
+        "My name is Pete"
+    end
+
+    get '/hometown' do 
+        "My hometown is Sajolad"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is "
+    end
+
+
+
 end
